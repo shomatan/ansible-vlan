470d5accc736f7bc2905054193f534f4986c2a09
Shoma Nishitateno <shoma416@gmail.com>
2017-02-03 20:29:33 +0900

    Repository

--- Account.scala
+++ Account.scala
@@ -39,3 +39,10 @@ object Account {
 trait AccountService {
   def transfer(from: Account, to: Account, amount: Amount): Option[Amount]
 }
+
+trait AccountRepository {
+  def query(accountNo: String): Option[Account]
+  def query(criteria: Criteria[Account]): Seq[Account]
+  def write(accounts: Seq[Account]): Boolean
+  def delete(account: Account): Boolean
+}
